ef2b8acc5126d0055310419932c083a331eadae9
Priyanka Khairnar-Borse <122878529+PriyankaKhairnar00@users.noreply.github.com>
2023-01-17 23:18:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const endDate = "28 July 2023 11:59 PM";
+
+document.getElementById("end-date").innerText= endDate;
+
+const inputs =document.querySelectorAll("input");
+
+function clock() {
+    const end = new Date(endDate);
+    const now = new Date();
+    const diff = (end - now)/1000;
+    if(diff<0)return;
+    
+    // console.log(diff);
+    inputs[0].value=(Math.floor(diff /3600 /24));
+    inputs[1].value=(Math.floor(diff /3600)% 24);
+    inputs[2].value=(Math.floor(diff /60) % 60);
+    inputs[3].value=(Math.floor(diff) % 60);
+    //convert into days
+}
+clock();
+
+// 1 day = 24hrs
+// 1 hr = 60mins
+// 60 min= 3600 sec
+
+setInterval(
+    ()=>{
+        clock()
+    },
+    1000
+)
